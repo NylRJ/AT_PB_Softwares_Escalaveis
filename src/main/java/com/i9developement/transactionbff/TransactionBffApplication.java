961a201cf218fd6972fe7a5ea2a25b12d9281518
Moises RZ <loveloungeoficial@gmail.com>
2021-04-24 06:53:02 -0300

    Create Project

---
+++ src/main/java/com/i9developement/transactionbff/TransactionBffApplication.java
@@ -0,0 +1,13 @@
+package com.i9developement.transactionbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionBffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/i9developement/transactionbff/TransactionBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i9developement.transactionbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

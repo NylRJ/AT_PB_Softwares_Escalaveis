788c85f63a4fcae3ba19c1ffe749105262e08b7a
Moises RZ <loveloungeoficial@gmail.com>
2021-04-29 14:15:16 -0300

    refactoring design by correcting artifact

---
+++ transaction-svc/src/main/java/com/i9developement/transactionsvc/TransactionSvcApplication.java
@@ -0,0 +1,13 @@
+package com.i9developement.transactionsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionSvcApplication.class, args);
+    }
+
+}
---
+++ transaction-svc/src/test/java/com/i9developement/transactionsvc/TransactionSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i9developement.transactionsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

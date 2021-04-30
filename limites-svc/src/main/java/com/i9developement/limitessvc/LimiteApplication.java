57075b087a275266755f6124d0b0c2bc08195aa1
Moises RZ <loveloungeoficial@gmail.com>
2021-04-30 03:32:26 -0300

    import dependencie

--- limites-svc/src/main/java/com/i9developement/limitessvc/LimiteApplication.java
+++ limites-svc/src/main/java/com/i9developement/limitessvc/LimiteApplication.java
@@ -2,8 +2,16 @@ package com.i9developement.limitessvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableCaching
+@EnableKafka
+@ComponentScan("com.i9developement")
 public class LimiteApplication {
 
     public static void main(String[] args) {

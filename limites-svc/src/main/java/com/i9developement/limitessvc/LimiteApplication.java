d5c0eb2f8a4822bbfca66d6c137beab75971d94e
Moises RZ <loveloungeoficial@gmail.com>
2021-04-30 03:24:32 -0300

    Create svc Limite

---
+++ limites-svc/src/main/java/com/i9developement/limitessvc/LimiteApplication.java
@@ -0,0 +1,13 @@
+package com.i9developement.limitessvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimiteApplication.class, args);
+    }
+
+}
---
+++ limites-svc/src/test/java/com/i9developement/limitessvc/LimiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i9developement.limitessvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
